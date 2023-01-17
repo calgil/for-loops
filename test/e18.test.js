fd5d3f8a3dcd2df0b707b3af2480606809481ada
calgil <clgilbert1105@gmail.com>
2023-01-17 15:37:24 -0700

    ex18 done

--- test/e18.test.js
+++ test/e18.test.js
@@ -2,11 +2,13 @@ import { describe, expect, it } from "vitest";
 import { joinToString } from "../exercises/e18";
 import { joinToStringData } from "../data/data";
 
-describe('isArrayIncludes', () => {
-  it('Should exist', () => {
+describe("isArrayIncludes", () => {
+  it("Should exist", () => {
     expect(joinToString).toBeInstanceOf(Function);
   });
-  it('Should return a correct string with symbols between array items values only', () => {
-    expect(joinToString(joinToStringData, ' ')).toEqual('Devslopes is teaching me FOR loops and functions!');
+  it("Should return a correct string with symbols between array items values only", () => {
+    expect(joinToString(joinToStringData, " ")).toEqual(
+      "Devslopes is teaching me FOR loops and functions !"
+    );
   });
 });

a61d6dbdf5d62cccb3ff4577792aa6f34ee3eda6
calgil <clgilbert1105@gmail.com>
2023-01-17 15:30:25 -0700

    fix ex16

--- exercises/e20.js
+++ exercises/e20.js
@@ -1,14 +1,9 @@
-
 // EXERCISE 20
 // Return and array of 2 arrays
 // (1 - an array of names that contain "a" in name, 2 - an array of names that don't have 'a' in name)
 // separateNamesWithAFromRest(["jon", "alice", "andrew", "mark", "jimmy"]) -> [["alice", "andrew", "mark"], ["jon", "jimmy"]]
 
-export function separateNamesWithAFromRest(array) {
-  // Your code goes here...
-
-}
-
+export function separateNamesWithAFromRest(array) {}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-20"
